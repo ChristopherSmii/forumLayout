fc61505d41355ec2f0d9a5a105511793ab0ba316
Βασίλης Χρήστος Σμυρλόγλου <94610215+ChristopherSmii@users.noreply.github.com>
2024-02-12 18:05:52 +0200

    Add files via upload

--- js/mainScript.js
+++ js/mainScript.js
@@ -4,7 +4,10 @@ const newTitleInput = document.getElementById("newPostTitle");
 const newTextInput = document.getElementById("newPostText");
 let newPostTitleIn;
 let newPostTextIn;
+const now = new Date();
 const forumContainer = document.getElementById("mainForum");
+const pastNotes = localStorage.getItem("mainNotes");
+forumContainer.innerHTML = pastNotes;
 $("#newForumButton").on("click", () => {
   document.querySelector("#newForum").classList.toggle("d-none");
 });
@@ -23,6 +26,7 @@ $("#postForumButton").on("click", () => {
   newPostTextIn = newTextInput.value;
   forumContainer.innerHTML =
     forumContainer.innerHTML + newPostFunction(newPostTitleIn, newPostTextIn);
+  localStorage.setItem("mainNotes", forumContainer.innerHTML);
   clearIn();
 });
 
@@ -39,7 +43,15 @@ const newPostFunction = (a, b) => {
   let line3 = "<h2>" + a + "</h2>";
   let line4 = '<p class="card-text">' + b + "</p>";
   let line5 =
-    '<button class="btn rounded-5 shadow"><i class="bi bi-plus"></i>Reply</button>';
+    '<h6 class="rounded-5" style="text-align:end;">' +
+    now.getDate() +
+    "/" +
+    (now.getMonth() + 1) +
+    " " +
+    now.getHours() +
+    ":" +
+    now.getMinutes() +
+    "</h6>";
   let line6 = "</div></div></div>";
   return line1 + line2 + line3 + line4 + line5 + line6;
 };
